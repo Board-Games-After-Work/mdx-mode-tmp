919b27dd9329d2c9eb1ec406b2973a5819c100ec
Nerd <hndppt@outlook.com>
2023-06-06 21:50:47 +0800

    dirs fmt

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import Page from "@comps/Page";
-import Preface from "@articles/Preface.mdx";
-
-export default () => (
-    <Page title="主页">
-        <Preface/>
-    </Page>
-);
--- pages/vol1.tsx
+++
@@ -1,8 +0,0 @@
-import Page from "@comps/Page";
-import Vol1 from "@articles/Vol1.mdx";
-
-export default () => (
-    <Page title="Vol.1: 艾尔萨托的陨落">
-        <Vol1 />
-    </Page>
-);
